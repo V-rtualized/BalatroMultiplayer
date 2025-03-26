61421f999b0f344d9d0b26d4283fff44bfc74ee5
Nanorasmus <nanorasm466u@gmail.com>
2025-03-26 04:12:39 +0100

    Updated configs to prepare for beta release

--- config.lua
+++ config.lua
@@ -1,7 +1,7 @@
 return {
 	["username"] = "Guest",
 	["server_url"] = "balatro.virtualized.dev",
-	["server_port"] = 8788,
+	["server_port"] = 6858,
 	["logging"] = false,
 	["misprint_display"] = true,
 }
